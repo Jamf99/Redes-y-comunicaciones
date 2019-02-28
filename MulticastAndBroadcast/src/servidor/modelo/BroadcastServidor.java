b8dea8581e39d9e5fcf81cc109fae68b8c18cb1c
Jorge Antonio Morales Florez <jamf_99@hotmail.com>
2019-02-28 10:37:22 -0500

    Actualizado

--- MulticastAndBroadcast/src/servidor/modelo/BroadcastServidor.java
+++ MulticastAndBroadcast/src/servidor/modelo/BroadcastServidor.java
@@ -1,13 +1,54 @@
 package servidor.modelo;
 
+import java.net.*;
+import java.nio.ByteBuffer;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import cliente.interfaz.VentanaCliente;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+
 public class BroadcastServidor {
-	
-	public BroadcastServidor() {
-		
+
+	private File leerArchivo() {
+		JFileChooser fileChooser = new JFileChooser();
+		int valor = fileChooser.showOpenDialog(fileChooser);
+		if (valor == JFileChooser.APPROVE_OPTION) {
+			String ruta = fileChooser.getSelectedFile().getAbsolutePath();
+			File f = new File(ruta);
+			return f;
+		} else {
+			return null;
+		}
+
 	}
-	
-	public void inicarEnvio() {
-		
+
+	public void enviarArchivo() throws Exception{
+
+		File imagen = leerArchivo();
+
+		if (imagen != null) {
+			Socket socket = new Socket("localhost", 13085);
+			OutputStream outputStream = socket.getOutputStream();
+
+			BufferedImage image = ImageIO.read(imagen);
+
+			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+			ImageIO.write(image, "jpg", byteArrayOutputStream);
+
+			byte[] size = ByteBuffer.allocate(4).putInt(byteArrayOutputStream.size()).array();
+			outputStream.write(size);
+			outputStream.write(byteArrayOutputStream.toByteArray());
+			outputStream.flush();
+			socket.close();
+		} else {
+			VentanaCliente.LOG("No se pudo leer la imagen");
+		}
+
 	}
 
 }
