d7dc8d68f760fa9919305b59d9c10a8e6693c215
alejandron00 <38502923+alejandron00@users.noreply.github.com>
2019-03-08 16:17:43 -0500

    medio multicast

--- MulticastAndBroadcast/src/cliente/modelo/MulticastCliente.java
+++ MulticastAndBroadcast/src/cliente/modelo/MulticastCliente.java
@@ -1,56 +1,42 @@
 package cliente.modelo;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.Socket;
 
 public class MulticastCliente {
-
-	public static void CrearArchivo(String mensaje) throws IOException {
-
-		String path="/ArchivoCliente/file.txt";
-		File archivo= new File(path);
-		FileWriter fl= new FileWriter(archivo);	
-				fl.write(mensaje);
-	
-	}
-	
 	public static void main(String[] args) {
+
 		boolean escuchar = true;
 		try {
 			System.setProperty("java.net.preferIPv4Stack", "true");
-			//para unir el cliente al grupo
+			// para unir el cliente al grupo
 			MulticastSocket mSock = new MulticastSocket(5000);
 			InetAddress dirGrupo = InetAddress.getByName("239.1.2.2");
 			mSock.joinGroup(dirGrupo);
-			
+
 			while (escuchar) {
-				//para recibir mensajes dirigidos al grupo
+				// para recibir mensajes dirigidos al grupo
 				byte[] buf = new byte[1000];
 				DatagramPacket recibe = new DatagramPacket(buf, buf.length);
 				mSock.receive(recibe);
-				
-				//para obtener los datos del mensaje
-				
-			
-				String datos = new String(recibe.getData(),0,recibe.getLength());
-				if(!datos.equals("FIN"))
-					CrearArchivo(datos);
+				// para obtener los datos del mensaje
+
+				// String datos = new String(recibe.getData(),recibe.getLength());
+				String datos = new String(recibe.getData(), 0, recibe.getLength());
+				if (!datos.equals("FIN"))
+					System.out.println(datos);
 				else
 					escuchar = false;
-				
-				//retirar cliente del grupo
-				
-				mSock.leaveGroup(dirGrupo);
-				
-				//cerrar multicast
-				mSock.close();
+
 			}
-			
-			
+
 		} catch (IOException e) {
 			System.err.println(e);
 		}
--- MulticastAndBroadcast/src/servidor/modelo/MulticastServidor.java
+++ MulticastAndBroadcast/src/servidor/modelo/MulticastServidor.java
@@ -1,87 +1,37 @@
 package servidor.modelo;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.io.*;
+import java.net.*;
+
+import javax.swing.JFileChooser;
 
 public class MulticastServidor {
-	
-	public boolean enviarMensaje(File archivo) {
-		boolean resultado = false;
-		
-		try {
-			DatagramSocket dSock = new DatagramSocket();
-			DatagramPacket dPacket = null;
-			InetAddress dirGrupo = InetAddress.getByName("239.1.2.2");
-		FileReader fr= new FileReader(archivo);
-		 BufferedReader bf=new BufferedReader(fr);
-		 String mensaje=bf.readLine();
-			if(mensaje != null) {
-				byte[] buf = mensaje.getBytes();
-				dPacket = new DatagramPacket(buf, buf.length,dirGrupo,5000);
-				dSock.send(dPacket);
-				resultado = true;
-			}
-			
-			dSock.close();
-		
-		} catch (IOException e) {
-			
-			System.out.println(e);
-		}
-		return resultado;
+
+
+	public static void enviar() throws IOException {
+		File f = new File("ArchivoClientes.txt");
+
+		DatagramSocket dSock = new DatagramSocket();
+		DatagramPacket dPacket = null;
+		InetAddress dirGrupo = InetAddress.getByName("239.1.2.2");
+
+		byte[] buf = new byte[(int) f.length()];
+		dPacket = new DatagramPacket(buf, buf.length, dirGrupo, 5000);
+		dSock.send(dPacket);
+		dSock.close();
+
 	}
-	public static File archivo() throws IOException {
-		String mensaje="";
-		String path="/ArchivoServidor/file.txt";
-		File archivo= new File(path);
-		FileWriter fl= new FileWriter(archivo);	
-				fl.write("LARVIS");
-				return archivo;
-		
-	}
-	
+
 	public static void main(String[] args) throws IOException {
-		
 		boolean continuar = true;
 		String mensaje = null;
-		
+
 		MulticastServidor server = new MulticastServidor();
 		while (continuar) {
-			System.out.println("Ingrese (1) para MENSAJE, Ingrese (2) para TERMINAR");
-			BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-			int opcion = Integer.parseInt(bf.readLine());
-			switch (opcion) {
-			case 1:
-				System.out.println("digite el mensaje a enviar!");
-				mensaje = bf.readLine();
-				if (server.enviarMensaje(archivo())) {
-					System.out.println("mensaje enviado");
-				}
-				else {
-					System.out.println("Error al enviar el mensaje");
-				}
-				break;
+			server.enviar();
 
-			case 2:
-				continuar = false;
-		//		server.enviarMensaje("FIN");
-				break;
-			
-			
-			default:
-				System.out.println("por favor seleccione una opcion correcta");
-				break;
-			
 		}
-		
-	}
 
 	}
+
 }
