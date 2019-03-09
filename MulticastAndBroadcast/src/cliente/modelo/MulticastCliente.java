772c47df28f346fc29b2c76233d6d58d78ccdeb7
alejandron00 <38502923+alejandron00@users.noreply.github.com>
2019-03-09 14:38:31 -0500

    .

--- MulticastAndBroadcast/src/cliente/modelo/MulticastCliente.java
+++ MulticastAndBroadcast/src/cliente/modelo/MulticastCliente.java
@@ -1,45 +1,62 @@
 package cliente.modelo;
 
 import java.io.BufferedInputStream;
+
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
+
 import java.io.IOException;
+
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.Socket;
 
-public class MulticastCliente extends Thread{
-	public static void main(String[] args) {
+public class MulticastCliente extends Thread {
 
-		boolean escuchar = true;
+	private MulticastSocket socket = null;
+	private byte[] buf = new byte[256];
+	private Socket socket1;
+
+	public void run() {
 		try {
-			System.setProperty("java.net.preferIPv4Stack", "true");
-			// para unir el cliente al grupo
-			MulticastSocket mSock = new MulticastSocket(5000);
-			InetAddress dirGrupo = InetAddress.getByName("239.1.2.2");
-			mSock.joinGroup(dirGrupo);
+			socket = new MulticastSocket(5000);
 
-			while (escuchar) {
-				// para recibir mensajes dirigidos al grupo
-				byte[] buf = new byte[1000];
-				DatagramPacket recibe = new DatagramPacket(buf, buf.length);
-				mSock.receive(recibe);
-				// para obtener los datos del mensaje
-				 
-				
-				// String datos = new String(recibe.getData(),recibe.getLength());
-				String datos = new String(recibe.getData(), 0, recibe.getLength());
-				if (!datos.equals("FIN"))
-					System.out.println(datos);
-				else
-					escuchar = false;
+			InetAddress group = InetAddress.getByName("239.1.2.2");
+			socket.joinGroup(group);
+			// --------------------------
+			// RECIBIR MENSAJE
+			// --------------------------
+			BufferedInputStream bis = new BufferedInputStream(socket1.getInputStream());
+			FileOutputStream fos = new FileOutputStream(new File("ArchivoClientes"));
 
+			int byteCounter = 0;
+			byte[] buffer = new byte[1024];
+
+			while ((byteCounter = bis.read(buffer)) != -1) {
+				fos.write(buffer, 0, byteCounter);
+				fos.flush();
 			}
 
+			fos.close();
+			// ------------------------------------------------------
+			while (true) {
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				socket.receive(packet);
+				String received = new String(packet.getData(), 0, packet.getLength());
+				if ("end".equals(received)) {
+					break;
+				}
+			}
+			// socket.leaveGroup(group);
+			// socket.close();
 		} catch (IOException e) {
-			System.err.println(e);
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+
+	public static void main(String[] args) {
+
 	}
 }
--- MulticastAndBroadcast/src/servidor/modelo/MulticastServidor.java
+++ MulticastAndBroadcast/src/servidor/modelo/MulticastServidor.java
@@ -6,14 +6,50 @@ import java.net.*;
 import javax.swing.JFileChooser;
 
 public class MulticastServidor {
-	
+
+	private static byte[] readBytesFromFile(String filePath) {
+
+		FileInputStream fileInputStream = null;
+		byte[] bytesArray = null;
+
+		try {
+
+			File file = new File(filePath);
+			bytesArray = new byte[(int) file.length()];
+
+			// read file into bytes[]
+			fileInputStream = new FileInputStream(file);
+			fileInputStream.read(bytesArray);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fileInputStream != null) {
+				try {
+					fileInputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+
+		return bytesArray;
+
+	}
+
 	public static void enviar() throws IOException {
 		File f = new File("ArchivoClientes.txt");
 		DatagramSocket dSock = new DatagramSocket();
 		DatagramPacket dPacket = null;
 		InetAddress dirGrupo = InetAddress.getByName("239.1.2.2");
 
-		byte[] buf = new byte[(int) f.length()];
+		// ----------------------
+		// MANDAR MENSJAE
+		// ----------------------
+		byte[] buf = readBytesFromFile(f.getPath());
+		// --------------------------
+
 		dPacket = new DatagramPacket(buf, buf.length, dirGrupo, 5000);
 		dSock.send(dPacket);
 		dSock.close();
